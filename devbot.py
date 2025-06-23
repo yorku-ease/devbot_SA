05199c8bdf70110fa5720af603f3e27db3ccabf5
Borna Ahmadzadeh <borna.ahz@gmail.com>
2025-06-23 09:46:15 -0400

    Add preliminary support for team-based flows

--- devbot.py
+++ devbot.py
@@ -1,3 +1,4 @@
+import re
 import os, subprocess, platform
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
@@ -6,22 +7,31 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from prompts import software_architecture_assistant_prompt_1, software_architecture_assistant_prompt_2
 from prompts import software_architecture_zero_shot_prompt, software_architecture_in_context_prompt, software_architecture_chain_of_thought_prompt
+from prompts import software_architecture_refine_prompt, software_architecture_fill_prompt, software_architecture_remove_prompt
 import pdfGenerator
 
 
-# this function gets called from the gui after user enter system description and model they want to use
-def query_llm(description, model, docType):
+def get_llm(model):
     if model in ["gpt-4o", "gpt-4o-mini", "o3", "o1"]:
         openai_api_key = load_api_key_from_file('openai_api_key')
         #instantiate the chosen model by the user
         llm = ChatOpenAI(api_key=openai_api_key,model= model, temperature=1)
-    elif model in ["gemini-2.0-flash", "gemini-1.5-pro"]: 
+    elif model in ["gemini-2.0-flash", "gemini-1.5-pro"]:
         google_api_key = load_api_key_from_file('google_api_key')
         llm = ChatGoogleGenerativeAI(model=model, google_api_key=google_api_key, temperature=1, top_p=0.8)
     else:
         groq_api_key = load_api_key_from_file('groq_api_key')
         llm = ChatGroq(api_key=groq_api_key, model_name=model, temperature=1, top_p=0.8)
+    return llm
 
+
+def last_puml_diagram(text):
+    return re.findall(r"```plaintext\n(.*?)```", text, re.DOTALL)
+
+
+# this function gets called from the gui after user enter system description and model they want to use
+def query_llm(description, model, docType, refine='', fill=False, remove=False):
+    llm = get_llm(model)
     llm_chain = ''
     fileName = ''
     if docType == "SDD":
@@ -44,6 +54,27 @@ def query_llm(description, model, docType):
     #query the chosen large language model and get the output.
     output = llm_chain.invoke({'description': description})['text']
 
+    if refine or fill or remove:
+        if refine:
+            prompt = software_architecture_refine_prompt
+        elif fill:
+            refine = 'gpt-4o'
+            prompt = software_architecture_fill_prompt
+        elif remove:
+            refine = 'llama-3.1-8b-instant'
+            prompt = software_architecture_remove_prompt
+
+        llm_refine = get_llm(refine)
+        llm_refine_chain = LLMChain(llm=llm_refine, prompt=prompt)
+        refined = llm_refine_chain.invoke({'description': description,
+                                           'llm_diagram': last_puml_diagram(output)})['text']
+        refined = last_puml_diagram(refined)
+
+        matches = list(re.finditer(r"```plaintext\n(.*?)```", output, re.DOTALL))
+        last_match = matches[-1]
+        start, end = last_match.span()
+        output = output[:start] + f"```plaintext\n{refined}```" + output[end:]
+
     # get directory where script is downloaded to create output file
     script_dir = os.path.dirname(os.path.realpath(__file__))
     output_file = os.path.join(script_dir, "output")
@@ -52,7 +83,6 @@ def query_llm(description, model, docType):
     with open(output_file, "w") as file:
         file.write(output)
 
-    
 
     # generate the software specification document in pdf form from gpt output file generated
     pdfGenerator.generate_pdf(output_file, script_dir, fileName)
--- prompts.py
+++ prompts.py
@@ -1,16 +1,16 @@
 from langchain.prompts import PromptTemplate
 
 software_architecture_assistant_template_1 = """
-You are a software architecture assistant chatbot named "Devbot". Your expertise is exclusively in designing software systems. 
+You are a software architecture assistant chatbot named "Devbot". Your expertise is exclusively in designing software systems.
 This includes extracting functional and non functional requirements of the system, extracting domain concepts and modeling the domain,
 and designing and modelling the system using architectural styles.
-I am providing the description of a system we want to design. Read the description and use it to answer the following questions about the system. 
+I am providing the description of a system we want to design. Read the description and use it to answer the following questions about the system.
 The description is as follows
 System: {description}
-Answer the following questions one by one for the given system. 
-1. identify the functional requirements for this system. Make sure the requirements are in the correct format. 
+Answer the following questions one by one for the given system.
+1. identify the functional requirements for this system. Make sure the requirements are in the correct format.
 2. Can you identify the non-functional requirements for this system. Make sure the requirements are in the correct format.
-3. Generate some use case scenarios involving the main functionalities of the system. Make sure the requirements are in the correct format with actors, precondition, main success scenario and postcondition 
+3. Generate some use case scenarios involving the main functionalities of the system. Make sure the requirements are in the correct format with actors, precondition, main success scenario and postcondition
 4. you are a domain expert. Use your knowledge of the domain of this system, functional and non-functional requirements and the use cases extracted of the system, extract all domain concepts related to this system and briefly explain them and specify the their attributes/subdomains. Do not attempt to model the system itself.
 5. Please list all relations of these domain concepts to each other. List one relation per line, output the concepts in PascalCase, like "DomainConceptA", and separate the related concepts in each line with "--". Do not explain the relations.  Make the list exhaustive.
 6. Draw a plantuml diagram with the concepts and relations. Keep it simple and do not include any attributes or methods.
@@ -25,18 +25,18 @@ software_architecture_assistant_prompt_1 = PromptTemplate(
 )
 
 software_architecture_assistant_template_2 = """
-You are a software architecture assistant chatbot named "Devbot". Your expertise is exclusively in designing software systems. 
-I am providing the description of a system we want to design. Read the description and use it to create a software specification document for the system. 
+You are a software architecture assistant chatbot named "Devbot". Your expertise is exclusively in designing software systems.
+I am providing the description of a system we want to design. Read the description and use it to create a software specification document for the system.
 The description is as follows
 System: {description}
 A software design document is a document containing detailed plan for building a software system. It covers the following areas, as specified in bullets:
 - Purpose
-- Sequence Diagrams: For each use case, introduce the corresponding PlantUML sequence diagram. Make sure you identify and associate each sequence diagram with the proper use case by maintaining unique Identifiers for use cases                                                                                                                                
+- Sequence Diagrams: For each use case, introduce the corresponding PlantUML sequence diagram. Make sure you identify and associate each sequence diagram with the proper use case by maintaining unique Identifiers for use cases
 - Major Design Decisions: Description of significant design choices, and modularization criteria. Discuss cohesion and coupling and two non-functional requirements that you focus on ( e.g, performance, security, reliability, cost, portability, etc..).
-- Architecture: Provide a discussion (two paragraphs in total) of the architectural patterns used, and the rationale behind your choice. Also provide an initial decomposition of your system as a collection of interacting modules in form of a PlantUML component diagram. Include explanations on the functionality of each component. Provide the exposed interfaces of each component and list and briefly describe the functionality (one sentence) of the operations included in each such interface. 
+- Architecture: Provide a discussion (two paragraphs in total) of the architectural patterns used, and the rationale behind your choice. Also provide an initial decomposition of your system as a collection of interacting modules in form of a PlantUML component diagram. Include explanations on the functionality of each component. Provide the exposed interfaces of each component and list and briefly describe the functionality (one sentence) of the operations included in each such interface.
 - Use of Design Patterns: In this section you will list each design pattern used and provide the rationale for each design pattern you used.
 - Detailed Class Diagrams: Detailed Plant UML class diagrams for each component in your system
-- Test Driven Development: Provide test cases in the following format: 
+- Test Driven Development: Provide test cases in the following format:
 1. Test ID:	The unique Id of the test case.   2. Category: Which part of the system is tested (e.g. evaluation of user  credentials stored on file or DB).  3. Initial Condition: Initial conditions required for the test case to run (e.g. the system has been initiated and runs). 4. Procedure: The list of steps required for this test case.  5. Expected Outcome: The expected outcome of the test case.
 Answer:"""
 
@@ -44,7 +44,7 @@ software_architecture_assistant_prompt_2 = PromptTemplate(
     input_variables=["description"],
     template=software_architecture_assistant_template_2
 )
-software_architecture_zero_shot_template = """ 
+software_architecture_zero_shot_template = """
 I am providing the description of a system we want to design. Create a high-level architecture for the system. Create a PlantUML component diagram (keep everything in a single package, Define Components like this "[Label] as Alias" and Connections like this "Alias1 --> Alias2" ) to model the architecture.
 The description is as follows
 System: {description}
@@ -54,7 +54,7 @@ software_architecture_zero_shot_prompt = PromptTemplate(
     input_variables=["description"],
     template=software_architecture_zero_shot_template
 )
-software_architecture_in_context_template = """ 
+software_architecture_in_context_template = """
 I am providing the description of a system we want to design. Create a high-level architecture for the system. Create a PlantUML component diagram (keep everything in a single package, Define Components like this "[Label] as Alias" and Connections like this "Alias1 --> Alias2" ) to model the architecture.
 Context: You are a software architect experienced in designing and implementing software systems in different domains given their descriptions. The attached text gives the description and core functionality of a software system. Generate an architecture for this system that: 1. Adheres to architecture styles and software design principles 2. Keep the archtiecture high level, excluding any implementation and technology specific details.
 The description is as follows
@@ -65,12 +65,12 @@ software_architecture_in_context_prompt = PromptTemplate(
     input_variables=["description"],
     template=software_architecture_in_context_template
 )
-software_architecture_chain_of_thought_template = """ 
+software_architecture_chain_of_thought_template = """
 I am providing the description of a system we want to design. Create a high-level architecture for the system. Create a PlantUML component diagram (keep everything in a single package, Define Components like this "[Label] as Alias" and Connections like this "Alias1 --> Alias2" ) to model the architecture by following these steps:
 1. Consider the functional and non-functional requirements of the system.
 2. Consider some main use cases of the system.
 3. Use your domain knowledge about the system and come up with a domain model.
-4. Using the domain modal, generate the high level architecture of the system, leaving out any low level implementation and technology specific details. 
+4. Using the domain modal, generate the high level architecture of the system, leaving out any low level implementation and technology specific details.
 The description is as follows
 System: {description}
 Answer:"""
@@ -78,4 +78,49 @@ Answer:"""
 software_architecture_chain_of_thought_prompt = PromptTemplate(
     input_variables=["description"],
     template=software_architecture_chain_of_thought_template
+)
+
+software_architecture_refine_template = """
+I am providing the description of a system we want to design. Additionally, I am also including a high-level architecture for the system generated by a large language model (LLM). Your task is to:
+1. Consider the functional and non-functional requirements of the system
+2. Analyze the LLM-generated architecture
+3. Identify any issues therein
+4. Rectify them and output the corrected code.
+The description is as follows
+System: {description}
+LLM-generated diagram: {llm_diagram}
+Answer:"""
+software_architecture_refine_prompt = PromptTemplate(
+    input_variables=["description", "llm_diagram"],
+    template=software_architecture_refine_template
+)
+
+software_architecture_fill_template = """
+I am providing the description of a system we want to design. Additionally, I am also including a high-level architecture for the system generated by a large language model (LLM). Your task is to:
+1. Consider the functional and non-functional requirements of the system
+2. Analyze the LLM-generated architecture
+3. Identify any *gaps* therein (that is, exclusively *missing* components and connections)
+4. *Fill* them and output the corrected code.
+The description is as follows
+System: {description}
+LLM-generated diagram: {llm_diagram}
+Answer:"""
+software_architecture_fill_prompt = PromptTemplate(
+    input_variables=["description", "llm_diagram"],
+    template=software_architecture_refine_template
+)
+
+software_architecture_remove_template = """
+I am providing the description of a system we want to design. Additionally, I am also including a high-level architecture for the system generated by a large language model (LLM). Your task is to:
+1. Consider the functional and non-functional requirements of the system
+2. Analyze the LLM-generated architecture
+3. Identify any *redundancies* therein (that is, exclusively *superfluous* components and connections)
+4. *Remove* them and output the corrected code.
+The description is as follows
+System: {description}
+LLM-generated diagram: {llm_diagram}
+Answer:"""
+software_architecture_remove_prompt = PromptTemplate(
+    input_variables=["description", "llm_diagram"],
+    template=software_architecture_refine_template
 )
